28d120b1ccd4efa6297f727355e531df9e9aa0a2
mervhountondji-boop <merv.hountondji@gmail.com>
2026-07-24 09:21:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
+let photo = document.getElementById("photo");
+let template = document.getElementById("template");
+
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+
+
+photo.onchange = function(){
+
+let fichier = photo.files[0];
+
+let reader = new FileReader();
+
+reader.onload = function(e){
+
+let img = new Image();
+
+img.onload=function(){
+
+canvas.width = template.width;
+canvas.height = template.height;
+
+
+// mettre le flyer
+ctx.drawImage(template,0,0);
+
+
+// mettre la photo dans le cercle
+ctx.save();
+
+ctx.beginPath();
+ctx.arc(540,700,150,0,Math.PI*2);
+ctx.clip();
+
+
+ctx.drawImage(img,390,550,300,300);
+
+ctx.restore();
+
+}
+
+img.src=e.target.result;
+
+}
+
+reader.readAsDataURL(fichier);
+
+}
+
+
+
 function download(){
 
-let image=document.getElementById("template");
-
 let lien=document.createElement("a");
-lien.href=image.src;
-lien.download="jy-serai-lim.png";
+
+lien.download="Jy-serai-LIM.png";
+
+lien.href=canvas.toDataURL();
+
 lien.click();
 
 }
