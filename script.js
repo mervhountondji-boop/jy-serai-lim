16938732c5a05b199402c3de62d198494afbf64a
mervhountondji-boop <merv.hountondji@gmail.com>
2026-07-24 10:25:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,26 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const template = new Image();
-template.src = "flyer.png";
-
-let userPhoto = null;
+let input = document.getElementById("photo");
+let preview = document.getElementById("photoPreview");
 
 
-// Charger le template
-template.onload = function(){
-    ctx.drawImage(template,0,0,1080,1350);
-}
+input.onchange = function(){
 
+let file = this.files[0];
 
-// Charger la photo
-document.getElementById("photo").onchange = function(e){
+let reader = new FileReader();
 
-const file = e.target.files[0];
+reader.onload=function(){
 
-const img = new Image();
-
-img.onload=function(){
-
-    // remettre le flyer
-    ctx.drawImage(template,0,0,1080,1350);
-
-
-    // cercle photo
-    ctx.save();
-
-    ctx.beginPath();
-    ctx.arc(540,850,220,0,Math.PI*2);
-    ctx.closePath();
-    ctx.clip();
-
-
-    // placer photo dans cercle
-    ctx.drawImage(img,320,630,440,440);
-
-
-    ctx.restore();
-
+preview.src = reader.result;
 
 }
 
-img.src=URL.createObjectURL(file);
+reader.readAsDataURL(file);
 
 }
 
-
-
-// téléchargement
 
 function download(){
 
-let lien=document.createElement("a");
-
-lien.download="jy-serai-lim.png";
-
-lien.href=canvas.toDataURL("image/png");
-
-lien.click();
+alert("Téléchargement activé après intégration finale du visuel");
 
 }
